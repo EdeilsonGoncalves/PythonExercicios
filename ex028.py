d4e3d6cfda6a1490082d9a2929eb89f31ce57a05
Edeilson <edeilsoniasd@outlook.com>
2024-01-15 21:07:23 -0300

    Condições

---
+++ ex028.py
@@ -0,0 +1,8 @@
+#Condições
+
+nome = str(input("Qual é seu nome? "))
+if nome == "Ed":
+    print("\nQue lindo nome você tem!")
+else:
+    print("\nSeu nome é tão normal!")
+print("\nBom dia, {}!".format(nome))
